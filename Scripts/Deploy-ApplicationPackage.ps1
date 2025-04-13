832f18d190d34e7b6ce0883122919092492040d9
Rasmus Buchholdt <rasmus.buchholdt@gmail.com>
2025-04-13 10:34:26 +0200

    Add support for skipFilesInput and skipPatternsInput

--- Scripts/Deploy-ApplicationPackage.ps1
+++ Scripts/Deploy-ApplicationPackage.ps1
@@ -1,5 +1,4 @@
-
-$msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe";
+$msdeploy = "C:\Program Files (x86)\IIS\Microsoft Web Deploy V3\msdeploy.exe"
 
 $source = $args[0]
 $destination = $args[1]
@@ -7,37 +6,51 @@ $recycleApp = $args[2]
 $computerName = $args[3]
 $username = $args[4]
 $password = $args[5]
-$delete = $args[6]
-$skipDirectory = $args[7]
+$delete = $args | Where-Object { $args.IndexOf($_) -eq 6 } | Select-Object -First 1
+$skipDirectory = $args | Where-Object { $args.IndexOf($_) -eq 7 } | Select-Object -First 1
+$skipFilesInput = $args | Where-Object { $args.IndexOf($_) -eq 8 } | Select-Object -First 1
+$skipPatternsInput = $args | Where-Object { $args.IndexOf($_) -eq 9 } | Select-Object -First 1
 
-$computerNameArgument = $computerName + '/MsDeploy.axd?site=' + $recycleApp
+$computerNameArgument = "$computerName/MsDeploy.axd?site=$recycleApp"
 
 $directory = Split-Path -Path (Get-Location) -Parent
 $baseName = (Get-Item $directory).BaseName
-$contentPath = Join-Path(Join-Path $directory $baseName) $source
+$contentPath = Join-Path (Join-Path $directory $baseName) $source
 
-$targetPath = $recycleApp + $destination
+$targetPath = "$recycleApp$destination"
 
 [System.Collections.ArrayList]$msdeployArguments = 
     "-verb:sync",
     "-allowUntrusted",
-    "-source:contentPath=${contentPath}," +
+    "-source:contentPath=$contentPath",
     ("-dest:" + 
-        "contentPath=${targetPath}," +
-        "computerName=${computerNameArgument}," + 
-        "username=${username}," +
-        "password=${password}," +
-        "AuthType='Basic'"
+      "contentPath=${targetPath}," +
+      "computerName=${computerNameArgument}," + 
+      "username=${username}," +
+      "password=${password}," +
+      "AuthType='Basic'"
     )
 
-if ($delete -NotMatch "true")
-{
-    $msdeployArguments.Add("-enableRule:DoNotDeleteRule")
+if ($delete -NotMatch "true") {
+  $msdeployArguments.Add("-enableRule:DoNotDeleteRule")
 }
 
-if ($skipDirectory)
-{
-    $msdeployArguments.Add("-skip:Directory=${skipDirectory}")
+if ($skipDirectory) {
+  $msdeployArguments.Add("-skip:Directory=$skipDirectory")
+}
+
+if ($skipFilesInput) {
+  $skipFiles = $skipFilesInput -split ","
+  foreach ($file in $skipFiles) {
+    $msdeployArguments.Add("-skip:File=$file")
+  }
+}
+
+if ($skipPatternsInput) {
+  $skipPatterns = $skipPatternsInput -split ","
+  foreach ($pattern in $skipPatterns) {
+    $msdeployArguments.Add("-skip:regexPattern=$pattern")
+  }
 }
 
 & $msdeploy $msdeployArguments
